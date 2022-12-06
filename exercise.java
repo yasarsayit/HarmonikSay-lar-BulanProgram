f36086791fd28c4e64f6cbba02679ea88a5a320f
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2022-12-06 21:27:03 +0300

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,22 @@
+package java101;
+
+import java.util.Scanner;
+
+public class exercise {
+
+	public static void main(String[] args) {
+
+		int number;
+		double total = 0.0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Sayı Giriniz: ");
+		number = input.nextInt();
+
+		for (double i = 1; i <= number; i++) {
+			total += (1 / i);
+		}
+		System.out.println("Sonuç: " + total);
+	}
+
+}
